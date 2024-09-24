fb30be28f82d3e68c4d06766e956c7c2815aaab2
Burak <burakkiran95@gmail.com>
2024-09-24 21:35:41 +0400

    Updated meta tags to fix some broken ones and add new ones

--- types/types.go
+++ types/types.go
@@ -39,8 +39,12 @@ func (bpd BlogPageDetails) GetMonthDateString() string {
 	return formatted
 }
 
-func (bpd BlogPageDetails) GetOgMetaTag() string {
+func (bpd BlogPageDetails) GetOgMetaTag(siteName string) string {
+	websiteLink := "https://%s%s"
+	pageUrl := fmt.Sprintf(websiteLink, siteName, bpd.Slug)
+	imgURL := fmt.Sprintf(websiteLink, siteName, bpd.Mainimgurl)
 	ogTagBase := "<meta property=\"%s\" content=\"%s\" />"
+	twitterTagBase := "<meta name=\"%s\" content=\"%s\" />"
 	var og_tags_sb strings.Builder
 	og_tags_sb.WriteString(fmt.Sprintf(ogTagBase, "og:type", "article"))
 	og_tags_sb.WriteString("\n")
@@ -50,10 +54,23 @@ func (bpd BlogPageDetails) GetOgMetaTag() string {
 	og_tags_sb.WriteString("\n")
 	og_tags_sb.WriteString(fmt.Sprintf(ogTagBase, "og:description", bpd.Snippet))
 	og_tags_sb.WriteString("\n")
-	og_tags_sb.WriteString(fmt.Sprintf(ogTagBase, "og:image", bpd.Mainimgurl))
+	// Fix this image size...
+	og_tags_sb.WriteString(fmt.Sprintf(ogTagBase, "og:image", imgURL))
 	og_tags_sb.WriteString("\n")
 	og_tags_sb.WriteString(fmt.Sprintf(ogTagBase, "og:image:width", "500"))
 	og_tags_sb.WriteString("\n")
 	og_tags_sb.WriteString(fmt.Sprintf(ogTagBase, "og:image:height", "500"))
+	og_tags_sb.WriteString("\n")
+	og_tags_sb.WriteString(fmt.Sprintf(ogTagBase, "og:site_name", "Integrand"))
+	og_tags_sb.WriteString("\n")
+	og_tags_sb.WriteString(fmt.Sprintf(ogTagBase, "og:url", pageUrl))
+	og_tags_sb.WriteString("\n")
+	og_tags_sb.WriteString(fmt.Sprintf(twitterTagBase, "twitter:card", "summary_large_image"))
+	og_tags_sb.WriteString("\n")
+	og_tags_sb.WriteString(fmt.Sprintf(twitterTagBase, "twitter:image", imgURL))
+	// <meta property="article:published_time" content="2019-01-08T06:00:59-05:00" />
+	// <meta property="article:modified_time" content="2024-06-18T15:57:17-05:00" />
+	// For facebook...
+	// <meta property="article:publisher" content="https://www.facebook.com/Backlinko"/>
 	return og_tags_sb.String()
 }
